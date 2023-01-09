3e628cdf9b51a8a6451109b266381c3e783c148d
carlesba <carlesba@users.noreply.github.com>
2023-01-09 11:14:41 +0100

    log version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+const version = "202301091111"
+
+console.log(version)
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
